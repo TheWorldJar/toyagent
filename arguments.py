41d483955a1456adb63af82a85eeac37f6a2b9c1
TheWorldJar <142929823+TheWorldJar@users.noreply.github.com>
2025-06-14 12:56:13 -0400

    Add arguments.py for command line argument management and refactor main.py to utilize the Commands enum for improved argument handling.

---
+++ arguments.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+
+class Commands(Enum):
+    """A list of the valid command line arguments that can be passed to the program."""
+
+    VERBOSE = "--verbose"
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from arguments import Commands
 
 
 def main():
@@ -11,12 +12,16 @@ def main():
             "Error: please provide a prompt when running this tool: python3 main.py {promt} --{argument}"
         )
         sys.exit(1)
-    be_verbose = False
-    if len(sys.argv) > 2 and sys.argv[2] != "--verbose":
-        print("Error: invalid argument: python3 main.py {promt} --{argument}")
-        sys.exit(1)
-    elif len(sys.argv) > 2 and sys.argv[2] == "--verbose":
-        be_verbose = True
+    command = None
+    if len(sys.argv) > 2:
+        match sys.argv[2]:
+            case Commands.VERBOSE.value:
+                command = Commands.VERBOSE
+            case _:
+                print("Error: command not recognized. Valid arguments are:\n")
+                for c in Commands:
+                    print(c.value)
+                sys.exit(1)
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
@@ -29,12 +34,15 @@ def main():
         contents=messages,
     )
     print(f"{response.text}\n")
-    if be_verbose:
-        print(
-            f"User prompt: {user_prompt}"
-            f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n"
-            f"Response tokens: {response.usage_metadata.candidates_token_count}\n"
-        )
+    match command:
+        case Commands.VERBOSE:
+            print(
+                f"User prompt: {user_prompt}\n"
+                f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n"
+                f"Response tokens: {response.usage_metadata.candidates_token_count}\n"
+            )
+        case _:
+            pass
 
 
 if __name__ == "__main__":
