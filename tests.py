ba3fa3c72071e669edde334f3e15ff8d40e25d06
TheWorldJar <142929823+TheWorldJar@users.noreply.github.com>
2025-06-15 14:49:47 -0400

    Changed tests to print to console to conform with Boot.dev testing requirements.

--- tests.py
+++ tests.py
@@ -7,42 +7,56 @@ class test_get_files_info(unittest.TestCase):
         test_dir = "."
         test_work_dir = "calculator"
         expected = f'Error: Cannot list "{test_dir}" as it is outside the permitted working directory'
-        self.assertEqual(get_files_info(test_work_dir, test_dir), expected)
+        test = get_files_info(test_work_dir, test_dir)
+        self.assertEqual(test, expected)
+        print(test)
 
     def test_work_is_dir(self):
         test_dir = "calculator"
         test_work_dir = "calculator"
         expected = "tests.py: file_size=1343 bytes, is_dir=False\nmain.py: file_size=576 bytes, is_dir=False\npkg: file_size=160 bytes, is_dir=True"
-        self.assertEqual(get_files_info(test_work_dir, test_dir), expected)
+        test = get_files_info(test_work_dir, test_dir)
+        self.assertEqual(test, expected)
+        print(test)
 
     def test_sub_in_work(self):
         test_dir = "pkg"
         test_work_dir = "calculator"
         expected = "render.py: file_size=768 bytes, is_dir=False\n__pycache__: file_size=128 bytes, is_dir=True\ncalculator.py: file_size=1739 bytes, is_dir=False"
-        self.assertEqual(get_files_info(test_work_dir, test_dir), expected)
+        test = get_files_info(test_work_dir, test_dir)
+        self.assertEqual(test, expected)
+        print(test)
 
     def test_root_outside_of_work(self):
         test_dir = "/bin"
         test_work_dir = "calculator"
         expected = f'Error: Cannot list "{test_dir}" as it is outside the permitted working directory'
-        self.assertEqual(get_files_info(test_work_dir, test_dir), expected)
+        test = get_files_info(test_work_dir, test_dir)
+        self.assertEqual(test, expected)
+        print(test)
 
     def test_parent_outside_work(self):
         test_dir = "../"
         test_work_dir = "calculator"
         expected = f'Error: Cannot list "{test_dir}" as it is outside the permitted working directory'
-        self.assertEqual(get_files_info(test_work_dir, test_dir), expected)
+        test = get_files_info(test_work_dir, test_dir)
+        self.assertEqual(test, expected)
+        print(test)
 
     def test_dir_is_file(self):
         test_dir = "tests.py"
         test_work_dir = "calculator"
         expected = f'Error: "{test_dir}" is not a directory'
-        self.assertEqual(get_files_info(test_work_dir, test_dir), expected)
+        test = get_files_info(test_work_dir, test_dir)
+        self.assertEqual(test, expected)
+        print(test)
 
     def test_only_working(self):
         test_work_dir = "calculator"
         expected = "tests.py: file_size=1343 bytes, is_dir=False\nmain.py: file_size=576 bytes, is_dir=False\npkg: file_size=160 bytes, is_dir=True"
-        self.assertEqual(get_files_info(test_work_dir), expected)
+        test = get_files_info(test_work_dir)
+        self.assertEqual(test, expected)
+        print(test)
 
 
 if __name__ == "__main__":
